b7b079b0480e3646c362d196ff65b8f607247fe2
nrenner <nrenner0211@gmail.com>
2022-05-15 23:37:13 -0400

    function added

--- script.js
+++ script.js
@@ -1,5 +1,46 @@
 // Assignment code here
+var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberChar = "0123456789";
+var specialChar = "U+0022, !#$%&'()*+,-./:;<=>?@[]\^_`{|}~";
 
+function generatePassword() {
+  var password = "";
+  var userPassword = "";
+
+  // select password length
+  var passwordLength = prompt("How many characters would you like your password to have? (8-128 characters)");
+  passwordLength = parseInt(passwordLength);
+
+  if (passwordLength < 8) {
+    alert("Password must have more than 7 characters!");
+    return "";
+  }
+
+  if (passwordLength > 128) {
+    alert("Password is too long!");
+    return "";
+  }
+
+  else {
+    alert("Not a valid option. Try again!");
+    return "";
+  }
+}
+
+// lowercaseChar yes/no
+var lowercaseCharChoice = confirm("Would you like some lowercase characters?");
+
+if (lowercaseCharChoice) {
+  userPassword += lowercaseChar;
+}
+
+// uppercaseChar yes/no
+var uppercaseCharChoice = confirm("How about uppercase characters?");
+
+if (uppercaseCharChoice) {
+  userPassword += uppercaseChar;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
